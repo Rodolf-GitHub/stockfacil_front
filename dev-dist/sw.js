4273447bedf6ae9d5d9260fe8e68cf89e9d870c0
Rodolfo <rodolfogroero2@gmail.com>
2026-04-22 15:57:55 -0300

    feat: actualizar revisiones de archivos de caché y reemplazar imágenes en la aplicación

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-52f2a342'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kmtbnr8pl94"
+    "revision": "0.fatj5ljd54s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
